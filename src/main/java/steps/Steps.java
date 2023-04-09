fca7e7483a8ad84a3ba4a12f80be19cb6b6ef5c9
Isaak <makarchuk008@gmail.com>
2023-04-09 19:29:57 +0400

    rework ReqSpec

--- src/main/java/steps/Steps.java
+++ src/main/java/steps/Steps.java
@@ -1,17 +1,30 @@
 package steps;
 
+import config.BaseConfig;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import lombok.RequiredArgsConstructor;
+import org.aeonbits.owner.ConfigFactory;
 import utilits.ResponseWrapper;
 
 import static io.restassured.RestAssured.given;
 
-@RequiredArgsConstructor
+
 public class Steps {
-    protected final RequestSpecification requestSpecification;
+
     private static final String BASE_URL = "pokemon/";
+
+    private final BaseConfig config = ConfigFactory.create(BaseConfig.class, System.getenv());
+
+    protected final RequestSpecification requestSpecification = new RequestSpecBuilder()
+            .setBaseUri(config.url())
+            .setContentType(ContentType.JSON)
+            .setAccept(ContentType.JSON)
+            .addFilter(new AllureRestAssured())
+            .build();
 
     @Step("GET запрос на получение данных покемона по имени")
     @Attachment(value = "Вложение", type = "application/json", fileExtension = ".txt")
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,27 +1,10 @@
-import config.BaseConfig;
-import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.aeonbits.owner.ConfigFactory;
 import steps.Steps;
 import utilits.ResponseWrapper;
 
 public class BaseTest {
 
-    private final BaseConfig config = ConfigFactory.create(BaseConfig.class, System.getenv());
-
-    protected final Steps steps = new Steps(getRequestSpecification());
+    protected final Steps steps = new Steps();
 
     protected ResponseWrapper responseWrapper;
-
-    private RequestSpecification getRequestSpecification() {
-        return new RequestSpecBuilder()
-                .setBaseUri(config.url())
-                .setContentType(ContentType.JSON)
-                .setAccept(ContentType.JSON)
-                .addFilter(new AllureRestAssured())
-                .build();
-    }
 
 }
